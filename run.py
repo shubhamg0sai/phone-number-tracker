2dd04357141e6a37bd75ad37b3bb4d6c01de8029
ʕ´•ᴥ•`ʔ╠ŞĦỮβĦa̷m̷╣ʕ´•ᴥ•`ʔ <63947251+shubhamg0sai@users.noreply.github.com>
2022-02-07 22:03:38 +0530

    Update run.py

--- run.py
+++ run.py
@@ -8,8 +8,10 @@ def banner():
    ██║   ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║
    ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
         \033[91mVersion - dev-1.1 coded by ShuBhamg0sai""")
+
 for i in range(2):
-        banner()
-        print("CTRL + z for exit")
-        no = input("\033[1;32m Enter number with country code\033[1;34m:\033[1;31m➤")
-        os.system("python2 tracker.py"+" "+no)
+    banner()
+    print("CTRL + z for exit")
+    cou = input("\033[1;32m Enter your country code\033[1;34m:\033[1;31m➤")
+    num = input("\033[1;32m Enter your number \033[1;34m:\033[1;31m➤")
+    os.system("python2 tracker.py"+" "+cou+num)
