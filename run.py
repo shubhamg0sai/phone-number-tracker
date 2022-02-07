6e2141b8567c89ac29205455b355aa88b9404f78
ʕ´•ᴥ•`ʔ╠ŞĦỮβĦa̷m̷╣ʕ´•ᴥ•`ʔ <63947251+shubhamg0sai@users.noreply.github.com>
2022-02-07 19:53:37 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import os
-no = input("\x1b[0;34m Enter Number With Country Code\x1b[1;91m➤ ")
+no = input("\033[1;32m Enter number with country code\033[1;34m:\033[1;31m➤")
 os.system("python2 tracker.py"+" "+no)
 os.system("python2 tracker.py"+" "+no)
