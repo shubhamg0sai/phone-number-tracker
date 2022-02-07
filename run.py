c081ff7ec5a56a07cf9141239b203436884e0b56
ʕ´•ᴥ•`ʔ╠ŞĦỮβĦa̷m̷╣ʕ´•ᴥ•`ʔ <63947251+shubhamg0sai@users.noreply.github.com>
2022-02-07 20:12:55 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 import os
-no = input("\033[1;32m Enter number with country code\033[1;34m:\033[1;31m➤")
-os.system("python2 tracker.py"+" "+no)
+def banner():
+        print  ("""\033[94m
+████████╗██████╗  █████╗  ██████╗██╗  ██╗███████╗██████╗
+╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗
+   ██║   ██████╔╝███████║██║     █████╔╝ █████╗  ██████╔╝
+   ██║   ██╔══██╗██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗
+   ██║   ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║
+   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+        \033[91mVersion - dev-1.1 coded by ShuBhamg0sai""")
+for i in range(2):
+        banner()
+        print("CTRL + z for exit")
+        no = input("\033[1;32m Enter number with country code\033[1;34m:\033[1;31m➤")
+        os.system("python2 tracker.py"+" "+no)
